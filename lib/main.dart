80435ab48262d58a92fcb34447a1ae045087cdf8
SimiAdeyemi <adeyems49@hotmail.com>
2023-03-30 14:50:40 +0100

    I.ve configured flutterfire so that the code works on various platforms

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:group_project/LogInScreens/SignInPage.dart';
 import 'package:flutter/material.dart';
 
 //MyApp is our root widget.
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized(); //makes sure Firebase is initialized before we run the app
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MyApp());
 }
 
 //This widget loads the SignInPage when app is run.
