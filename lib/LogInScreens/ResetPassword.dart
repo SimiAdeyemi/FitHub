b8f1667ae1666ec53a8347384c7d330d64223bd8
Theo Innis <sgtinnis@liverpool.ac.uk>
2023-04-12 10:26:38 +0100

    In ResetPassword.dart, error message shown to user for incorrect email format. If user inputs email of a user that doesn't exist, an error message appears at bottom of screen.

--- lib/LogInScreens/ResetPassword.dart
+++ lib/LogInScreens/ResetPassword.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import '../ReusableWidgets.dart';
 
 class ResetPassword extends StatefulWidget {
@@ -13,6 +12,8 @@ class ResetPassword extends StatefulWidget {
 
 class _ResetPasswordState extends State<ResetPassword> {
   TextEditingController _emailTextController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,28 +27,74 @@ class _ResetPasswordState extends State<ResetPassword> {
         ),
       ),
       body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: SingleChildScrollView(
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(20, 120, 20, 0),
-                child: Column(
-                  children: <Widget>[
-                    const SizedBox(
-                      height: 20,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(20, 120, 20, 0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    controller: _emailTextController,
+                    decoration: InputDecoration(
+                      labelText: "Enter Email",
+                      prefixIcon: Icon(Icons.email_outlined),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
                     ),
-                    reusableTextField("Enter Email", Icons.email_outlined, false,
-                        _emailTextController),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    firebaseButton(context, "Reset Password", () { //when pressed a "reset password" email is sent
-                      FirebaseAuth.instance.sendPasswordResetEmail(email: _emailTextController.text)
-                          .then((value) => Navigator.of(context).pop());
-                    })
-                  ],
-                ),
-              ))),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your email.';
+                      } else if (!RegExp(r'^[\w-]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value)) {
+                        return 'Please enter a valid email address.';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  firebaseButton(context, "Reset Password", () async {
+                    if (_formKey.currentState!.validate()) {
+                      try {
+                        await FirebaseAuth.instance.sendPasswordResetEmail(email: _emailTextController.text);
+                        Navigator.of(context).pop();
+                      } on FirebaseAuthException catch (e) {
+                        if (e.code == 'user-not-found') {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('This email is not registered.'),
+                            ),
+                          );
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('There was an error resetting the password. Please try again later.'),
+                            ),
+                          );
+                        }
+                      } catch (e) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text('There was an error resetting the password. Please try again later.'),
+                          ),
+                        );
+                      }
+                    }
+                  })
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
+
