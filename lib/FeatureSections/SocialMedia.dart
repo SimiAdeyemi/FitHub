e3a93341969cb6e69be4a423239ab11cb6e7b192
jeruboateng <sgjboate@liverpool.ac.uk>
2023-04-17 23:10:21 +0100

    having trouble uploading posts to firebase

--- lib/FeatureSections/SocialMedia.dart
+++ lib/FeatureSections/SocialMedia.dart
@@ -1,31 +1,112 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class SocialMedia extends StatelessWidget {
+class SocialMedia extends StatefulWidget {
   const SocialMedia({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  _SocialMediaState createState() => _SocialMediaState();
+}
+
+
+
+
+class _SocialMediaState extends State<SocialMedia> {
+  File? file;
+
+  takePhoto() async {
+    XFile? post = await ImagePicker().pickImage(
+      source: ImageSource.camera,
+      maxHeight: 1080,
+      maxWidth: 1080,
+    );
+    if (post != null) {
+      setState(() {
+        file = File(post.path);
+      });
+      //then we display picture send it to database then set file to null
+    }
+  }
+
+  takeFromGallery() async {
+    XFile? post = await ImagePicker().pickImage(
+      source: ImageSource.gallery,
+      maxHeight: 1080,
+      maxWidth: 1080,
+    );
+    if (post != null) {
+      setState(() {
+        file = File(post.path);
+      });
+    }
+  }
+
+  //https://stacksecrets.com/flutter/build-an-image-picker-wrapper-widget-in-flutter
+  //https://stackoverflow.com/questions/49809351/how-to-create-a-circle-icon-button-in-flutter
+
+  Scaffold postFunction(BuildContext context) {
     return Scaffold(
       body: Container(
         alignment: Alignment.bottomCenter,
         child: ElevatedButton(
-          child: const Text("Upload"),
           onPressed: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return SimpleDialog(
+                  title: const Text("Upload"),
+                  children: <Widget>[
+                    SimpleDialogOption(
+                      onPressed: () async {
+                        Navigator.pop(context); // close the dialog box
+                        takePhoto();
+                      },
+                      child: const Text('Camera'),
+                    ),
+                    SimpleDialogOption(
+                      onPressed: () async {
+                        Navigator.pop(context);
+                        takeFromGallery(); // close the dialog box
+                      },
+                      child: const Text('Camera Roll'),
+                    ),
+                    SimpleDialogOption(
+                      onPressed: () async {
+                        Navigator.pop(context); // close the dialog box
+                      },
+                      child: const Text('Cancel'),
+                    ),
+                  ],
+                );
+              },
+            );
           },
           style: ElevatedButton.styleFrom(
+            shape: const CircleBorder(),
             backgroundColor: Colors.green,
             elevation: 0,
           ),
-
+          child: const Icon(
+            Icons.camera_alt,
+            size: 24,
+          ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.green,
+        onPressed: () {},
+        child: const Icon(Icons.person),
       ),
     );
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return postFunction(context);
+  }
+}
 
 
 
 
-
-
-}//entire class
