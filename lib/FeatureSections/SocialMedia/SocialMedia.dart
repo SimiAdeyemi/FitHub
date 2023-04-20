1a1dded9bd9cbc322e7074f9476dc4239427fd0b
Theo Innis <sgtinnis@liverpool.ac.uk>
2023-04-20 13:58:05 +0100

    A user selects image and after an image is selected a preview pop-up appears, so a user can choose to post or cancel. If user posts photos, then it gets saved in Firebase Storage in images/. The imageUrl and displayName is saved into Firestore in "Social Media Posts" collection.

--- lib/FeatureSections/SocialMedia/SocialMedia.dart
+++ lib/FeatureSections/SocialMedia/SocialMedia.dart
@@ -1,15 +1,9 @@
 import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:group_project/globals.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-//import 'package:firebase_core/firebase_core.dart';
-
-//line to make code different for push
-
-//to do list
-//1. uploads posts to data base
-//2. display all the posts
 
 class SocialMedia extends StatefulWidget {
   const SocialMedia({Key? key}) : super(key: key);
@@ -21,19 +15,38 @@ class SocialMedia extends StatefulWidget {
 class _SocialMediaState extends State<SocialMedia> {
   File? file;
 
-  Future<void> uploadImage(File file) async {
-    // Generate a unique path using the current time in milliseconds
-    final path =
-        'posts/${DateTime.now().millisecondsSinceEpoch}-$displayName-my-image';
-
-    // Upload the file to Firebase Storage
-    final ref = FirebaseStorage.instance.ref().child(path);
-    await ref.putFile(file);
-    String url = await ref.getDownloadURL();
-    createPostInFireStore(url);
+  @override
+  Widget build(BuildContext context) {
+    //return file == null ? postFunction(context) : buildUploadForm();
+    return postFunction(context);
   }
 
-  createPostInFireStore(String postUrl) {}
+  Future<void> uploadImage(File file) async {
+    try {
+      // Generate a unique path using the current time in milliseconds
+      final path = 'images/${DateTime.now().millisecondsSinceEpoch}-$displayName-my-image';
+
+      // Get a reference to the Firebase Storage location to upload the file
+      final Reference ref = FirebaseStorage.instance.ref().child(path);
+
+      // Upload the file to Firebase Storage
+      await ref.putFile(file);
+
+      // Get the download URL of the uploaded file
+      final String downloadUrl = await ref.getDownloadURL();
+
+      // Save the download URL and the display name to Firestore
+      await FirebaseFirestore.instance.collection('Social Media Posts').add({
+        'imageUrl': downloadUrl,
+        'displayName': displayName,
+      });
+    } catch (e) {
+      // Handle any errors that might occur
+      print(e.toString());
+    }
+  }
+
+  //createPostInFireStore(String postUrl) {}
 
   takePhoto() async {
     XFile? post = await ImagePicker().pickImage(
@@ -45,7 +58,35 @@ class _SocialMediaState extends State<SocialMedia> {
       setState(() {
         file = File(post.path);
         debugPrint("jeru File path: ${file?.path}");
-        //uploadImage(file!); // Upload the file to Firebase Storage
+        // Show a popup dialog with the selected image and "Post" and "Cancel" buttons
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Preview Image'),
+              content: Container(
+                width: double.maxFinite,
+                child: Image.file(file!),
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context); // Close the dialog box
+                  },
+                  child: const Text('Cancel'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context); // Close the dialog box
+                    // Upload the file to Firebase Storage and save the download URL
+                    uploadImage(file!);
+                  },
+                  child: const Text('Post'),
+                ),
+              ],
+            );
+          },
+        );
       });
     }
   }
@@ -59,13 +100,38 @@ class _SocialMediaState extends State<SocialMedia> {
     if (post != null) {
       setState(() {
         file = File(post.path);
-        // uploadImage(file!); // Upload the file to Firebase Storage
+        // Show a popup dialog with the selected image and "Post" and "Cancel" buttons
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Preview Image'),
+              content: Container(
+                width: double.maxFinite,
+                child: Image.file(file!),
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context); // Close the dialog box
+                  },
+                  child: const Text('Cancel'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context); // Close the dialog box
+                    // Upload the file to Firebase Storage and save the download URL
+                    uploadImage(file!);
+                  },
+                  child: const Text('Post'),
+                ),
+              ],
+            );
+          },
+        );
       });
     }
   }
-
-  //https://stacksecrets.com/flutter/build-an-image-picker-wrapper-widget-in-flutter
-  //https://stackoverflow.com/questions/49809351/how-to-create-a-circle-icon-button-in-flutter
 
   Scaffold postFunction(BuildContext context) {
     return Scaffold(
@@ -121,52 +187,5 @@ class _SocialMediaState extends State<SocialMedia> {
         child: const Icon(Icons.person),
       ),
     );
-  }
-
-  clearImage() {
-    setState(() {
-      file = null;
-    });
-  }
-
-  Scaffold buildUploadForm() {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        // center the title
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.black),
-          onPressed: clearImage,
-        ),
-        title: const Text(
-          "Upload Post",
-          style: TextStyle(color: Colors.black),
-        ),
-        actions: [
-          TextButton(
-            onPressed: () {
-              if (file != null) {
-                uploadImage(file!);
-              }
-            },
-            child: const Text(
-              "Post",
-              style: TextStyle(
-                color: Colors.green,
-                fontWeight: FontWeight.bold,
-                fontSize: 20.0,
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-
-  @override
-  Widget build(BuildContext context) {
-    return file == null ? postFunction(context) : buildUploadForm();
   }
 }
