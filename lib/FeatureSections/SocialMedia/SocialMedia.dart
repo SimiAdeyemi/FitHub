90afd4d0ba70ceedfb007163ca10adb2db886138
Dawid Rybinski <42513264+dawid3201@users.noreply.github.com>
2023-04-20 16:14:40 +0100

    Social Media posts can be added

--- lib/FeatureSections/SocialMedia/SocialMedia.dart
+++ lib/FeatureSections/SocialMedia/SocialMedia.dart
@@ -1,15 +1,9 @@
 import 'dart:io';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:group_project/globals.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-//import 'package:firebase_core/firebase_core.dart';
-
-//line to make code different for push
-
-//to do list
-//1. uploads posts to data base
-//2. display all the posts
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SocialMedia extends StatefulWidget {
   const SocialMedia({Key? key}) : super(key: key);
@@ -20,20 +14,75 @@ class SocialMedia extends StatefulWidget {
 
 class _SocialMediaState extends State<SocialMedia> {
   File? file;
+  bool isUploading = false;
+  String? imageUrl;
+  final TextEditingController descriptionController = TextEditingController();
+  final CollectionReference postsRef = FirebaseFirestore.instance.collection('posts');
+  List<Post> posts = [];
+  FirebaseAuth get auth => FirebaseAuth.instance;
+
+
+
+  @override
+  void initState() {
+    super.initState();
+    fetchPosts();
+  }
+
 
   Future<void> uploadImage(File file) async {
-    // Generate a unique path using the current time in milliseconds
     final path =
-        'posts/${DateTime.now().millisecondsSinceEpoch}-$displayName-my-image';
+        'posts/${DateTime.now().millisecondsSinceEpoch}-yourUsername-my-image';
 
-    // Upload the file to Firebase Storage
     final ref = FirebaseStorage.instance.ref().child(path);
     await ref.putFile(file);
     String url = await ref.getDownloadURL();
     createPostInFireStore(url);
   }
 
-  createPostInFireStore(String postUrl) {}
+  Future<void> createPostInFireStore(String postUrl) async {
+    final User? currentUser = auth.currentUser;
+
+    if (currentUser != null) {
+      DocumentReference docRef = FirebaseFirestore.instance.collection('posts').doc();
+      final String postId = docRef.id;
+      final String userId = currentUser.uid;
+      final String username = currentUser.displayName ?? 'Anonymous';
+      final String description = descriptionController.text;
+
+      await docRef.set({
+        'postId': postId,
+        'userId': userId,
+        'username': username,
+        'imageUrl': postUrl,
+        'description': description,
+        'timestamp': DateTime.now(),
+      });
+
+      setState(() {
+        imageUrl = postUrl;
+        isUploading = false;
+        file = null;
+        descriptionController.clear();
+      });
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('You need to be logged in to create a post')),
+      );
+    }
+  }
+
+
+
+  Future<List<Post>> fetchPosts() async {
+    QuerySnapshot querySnapshot = await postsRef
+        .orderBy('timestamp', descending: true)
+        .get();
+    return querySnapshot.docs.map((doc) => Post.fromDocument(doc)).toList();
+  }
+
+
+
 
   takePhoto() async {
     XFile? post = await ImagePicker().pickImage(
@@ -44,8 +93,6 @@ class _SocialMediaState extends State<SocialMedia> {
     if (post != null) {
       setState(() {
         file = File(post.path);
-        debugPrint("jeru File path: ${file?.path}");
-        //uploadImage(file!); // Upload the file to Firebase Storage
       });
     }
   }
@@ -59,114 +106,203 @@ class _SocialMediaState extends State<SocialMedia> {
     if (post != null) {
       setState(() {
         file = File(post.path);
-        // uploadImage(file!); // Upload the file to Firebase Storage
       });
     }
   }
+  Widget buildPostWidget(Post post) {
+    return Column(
+      children: [
+        ListTile(
+          title: Text(post.username),
+          subtitle: Text(post.description),
+          trailing: const Icon(Icons.more_vert),
+        ),
+        Image.network(post.imageUrl),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: const [
+            Padding(
+              padding: EdgeInsets.only(left: 20.0, top: 10.0),
+            ),
+            Icon(Icons.favorite_border, size: 28.0),
+            SizedBox(width: 20.0),
+            Icon(Icons.chat_bubble_outline, size: 28.0),
+          ],
+        ),
+        const Divider(),
+      ],
+    );
+  }
 
-  //https://stacksecrets.com/flutter/build-an-image-picker-wrapper-widget-in-flutter
-  //https://stackoverflow.com/questions/49809351/how-to-create-a-circle-icon-button-in-flutter
 
   Scaffold postFunction(BuildContext context) {
     return Scaffold(
-      body: Container(
-        alignment: Alignment.bottomCenter,
-        child: ElevatedButton(
-          onPressed: () {
-            showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return SimpleDialog(
-                  title: const Text("Upload"),
-                  children: <Widget>[
-                    SimpleDialogOption(
-                      onPressed: () async {
-                        Navigator.pop(context); // close the dialog box
-                        takePhoto();
-                      },
-                      child: const Text('Camera'),
-                    ),
-                    SimpleDialogOption(
-                      onPressed: () async {
-                        Navigator.pop(context);
-                        takeFromGallery(); // close the dialog box
-                      },
-                      child: const Text('Camera Roll'),
-                    ),
-                    SimpleDialogOption(
-                      onPressed: () async {
-                        Navigator.pop(context); // close the dialog box
-                      },
-                      child: const Text('Cancel'),
-                    ),
-                  ],
-                );
-              },
-            );
+      body: RefreshIndicator(
+        onRefresh: () async {
+          setState(() {});
+        },
+        child: FutureBuilder(
+          future: fetchPosts(),
+          builder: (BuildContext context, AsyncSnapshot<List<Post>> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return const Center(child: Text('Failed to load posts'));
+            } else {
+              List<Post> posts = snapshot.data!;
+              return ListView.builder(
+                itemCount: posts.length,
+                itemBuilder: (BuildContext context, int index) {
+                  Post post = posts[index];
+                  return buildPostWidget(post);
+                },
+              );
+            }
           },
-          style: ElevatedButton.styleFrom(
-            shape: const CircleBorder(),
-            backgroundColor: Colors.green,
-            elevation: 0,
-          ),
-          child: const Icon(
-            Icons.camera_alt,
-            size: 24,
-          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return SimpleDialog(
+                title: const Text("Upload"),
+                children: <Widget>[
+                  SimpleDialogOption(
+                    onPressed: () async {
+                      Navigator.pop(context);
+                      takePhoto();
+                    },
+                    child: const Text('Camera'),
+                  ),
+                  SimpleDialogOption(
+                    onPressed: () async {
+                      Navigator.pop(context);
+                      takeFromGallery();
+                    },
+                    child: const Text('Camera Roll'),
+                  ),
+                  SimpleDialogOption(
+                    onPressed: () async {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Cancel'),
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        child: const Icon(Icons.add),
         backgroundColor: Colors.green,
-        onPressed: () {},
-        child: const Icon(Icons.person),
       ),
     );
   }
 
-  clearImage() {
-    setState(() {
-      file = null;
-    });
-  }
 
-  Scaffold buildUploadForm() {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        // center the title
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.black),
-          onPressed: clearImage,
-        ),
-        title: const Text(
-          "Upload Post",
-          style: TextStyle(color: Colors.black),
-        ),
-        actions: [
-          TextButton(
-            onPressed: () {
-              if (file != null) {
-                uploadImage(file!);
-              }
-            },
-            child: const Text(
-              "Post",
-              style: TextStyle(
+
+  clearImage() {
+          setState(() {
+            file = null;
+          });
+        }
+    Scaffold buildUploadForm() {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back, color: Colors.black),
+            onPressed: clearImage,
+          ),
+          title: const Text(
+            "Upload Post",
+            style: TextStyle(color: Colors.black),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                if (file != null) {
+                  uploadImage(file!);
+                }
+              },
+              child: const Text(
+                "Post",
+                style: TextStyle(
                 color: Colors.green,
                 fontWeight: FontWeight.bold,
                 fontSize: 20.0,
+                ),
               ),
             ),
+          ],
+        ),
+        body: ListView(
+          children: <Widget>[
+            Container(
+              height: 220.0,
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: Center(
+                child: AspectRatio(
+                  aspectRatio: 16 / 9,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: FileImage(file!),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const Padding(
+            padding: EdgeInsets.only(top: 10.0),
+            ),
+            TextFormField(
+            controller: descriptionController,
+            decoration: const InputDecoration(
+            hintText: "Write a description...",
+            border: OutlineInputBorder(),
+            ),
+            ),
+          ],
           ),
-        ],
-      ),
-    );
+      );
+    }
+
+        @override
+        Widget build(BuildContext context) {
+      return file == null ? postFunction(context) : buildUploadForm();
+    }
   }
 
+  class Post {
+    final String postId;
+    final String userId;
+    final String username;
+    final String imageUrl;
+    final String description;
+    final DateTime timestamp;
 
-  @override
-  Widget build(BuildContext context) {
-    return file == null ? postFunction(context) : buildUploadForm();
+    Post({
+    required this.postId,
+    required this.userId,
+    required this.username,
+    required this.imageUrl,
+    required this.description,
+    required this.timestamp,
+    });
+
+    factory Post.fromDocument(DocumentSnapshot doc) {
+      return Post(
+        postId: doc['postId'],
+        userId: doc['userId'],
+        username: doc['username'],
+        imageUrl: doc['imageUrl'],
+        description: doc['description'],
+        timestamp: (doc['timestamp'] as Timestamp).toDate(),
+      );
+    }
   }
-}
--- lib/globals.dart
+++ lib/globals.dart
@@ -1,3 +1,4 @@
 library my_libraray.globals;
 
 String displayName = ''; //global variable
+String? imageUrl;
